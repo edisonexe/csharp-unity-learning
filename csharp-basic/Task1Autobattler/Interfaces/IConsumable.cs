8fd6a45ea385e5e587726a3220007bb0b93293ca
Ilia Rodkin <rodkinilia@mail.ru>
2025-10-31 22:43:37 +0700

    feat: Add interfaces IConsumable and IEquippable

---
+++ csharp-basic/Task1Autobattler/Interfaces/IConsumable.cs
@@ -0,0 +1,8 @@
+﻿using Task1Autobattler.Characters;
+
+namespace Task1Autobattler.Interfaces;
+
+public interface IConsumable
+{
+    void Consume(Player player);
+}
---
+++ csharp-basic/Task1Autobattler/Interfaces/IEquippable.cs
@@ -0,0 +1,9 @@
+﻿using Task1Autobattler.Characters;
+
+namespace Task1Autobattler.Interfaces;
+
+public interface IEquippable
+{
+    public void Equip(Player player);
+    public void Unequip(Player player);
+}
