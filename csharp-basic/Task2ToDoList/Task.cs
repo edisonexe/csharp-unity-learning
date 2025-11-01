27d26db52c41f816fc313eb86a17dc1d35f0c917
Ilia Rodkin <rodkinilia@mail.ru>
2025-11-01 16:03:49 +0700

    feat(task2): Add class Task

---
+++ csharp-basic/Task2ToDoList/Task.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2ToDoList;
+
+public class Task
+{
+    public string Name { get; }
+    public string? Description { get; }
+    public Priority Priority { get; }
+    public Category Category { get; }
+    public Status Status { get; private set; }
+
+    public Task(string name, string description, Priority priority, Category category, Status status)
+    {
+        Name = name;
+        Description = description;
+        Priority = priority;
+        Category = category;
+        Status = status;
+    }
+
+    public void Complete() => Status = Status.Done;
+}
