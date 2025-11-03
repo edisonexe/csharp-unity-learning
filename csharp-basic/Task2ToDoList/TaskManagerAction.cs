ed288c2cb5b916c759044e9b9146db76eda2a12c
Ilia Rodkin <rodkinilia@mail.ru>
2025-11-04 00:20:56 +0700

    feat(task2): Add TaskManagerAction enum for menu actions

---
+++ csharp-basic/Task2ToDoList/TaskManagerAction.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2ToDoList;
+
+public enum TaskManagerAction
+{
+    Exit = 0,
+    AddTask = 1,
+    ShowAllTasks,
+    FilterByStatus,
+    FilterByCategory,
+    MarkAsDone,
+    DeleteByIndex
+}
