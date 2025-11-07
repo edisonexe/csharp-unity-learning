48518c80b09dc1ed34899239bc7fd59ad735c853
Ilia Rodkin <rid843@mail.ru>
2025-11-07 19:39:55 +0700

    feat: Add target rotation and scaling

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,18 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    [SerializeField] private float _timeToDestroy = 3f;
+    [SerializeField] private float _rotationSpeed = 20f;
     private float _lifeTime;
     private float _logTick;
+    private Coroutine _scaleCoroutine;
+    
+    [Header("Scaling")]
+    [SerializeField] private float _minScale = 1f;
+    [SerializeField] private float _maxScale = 2f;
+    [SerializeField] private float _scaleSpeed = 1f;
+    private bool _scalingUp = true;
     
     private void Start()
     {
         Debug.Log("Target created.");
-        Destroy(gameObject, 3f);
+        _scaleCoroutine = StartCoroutine(Scale());
+        Destroy(gameObject, _timeToDestroy);
     }
 
     private void Update()
     {
+        transform.Rotate(0, _rotationSpeed * Time.deltaTime,0);
         _lifeTime += Time.deltaTime;
         _logTick += Time.deltaTime;
         if (_logTick >= 0.99f)
@@ -22,5 +34,29 @@ public class Target : MonoBehaviour
         }
     }
 
-    private void OnDestroy() => Debug.Log("Target destroyed.");
+    private void OnDestroy()
+    {
+        Debug.Log("Target destroyed.");
+        if (_scaleCoroutine != null)
+            StopCoroutine(_scaleCoroutine);
+    }
+
+    private IEnumerator Scale()
+    {
+        while (true)
+        {
+            float targetScale = _scalingUp ? _maxScale : _minScale;
+            Vector3 startScale = transform.localScale;
+            Vector3 endScale = Vector3.one * targetScale;
+            float scaleProgress = 0f;
+            
+            while (scaleProgress < 1f)
+            {
+                scaleProgress += Time.deltaTime * _scaleSpeed;
+                transform.localScale = Vector3.Lerp(startScale, endScale, scaleProgress);
+                yield return null;
+            }
+            _scalingUp = !_scalingUp;
+        }
+    }
 }
