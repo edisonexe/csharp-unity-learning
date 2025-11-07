9333b5f3e66681d274a30383a134f387a48cdb6e
Ilia Rodkin <rid843@mail.ru>
2025-11-07 19:47:12 +0700

    feat: Add targets spawned text on UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject _targetPrefab;
     [SerializeField] private float _spawnInterval = 4f;
+    [SerializeField] private Text _targetsSpawnedText;
     private Camera _cam;
-    private static int _totalTargetsSpawned;
+    private int _totalTargetsSpawned = 0;
     
     private void Start()
     {
         _cam = Camera.main;
-        _totalTargetsSpawned = 0;
-        
         InvokeRepeating(nameof(SpawnTarget), 0f, _spawnInterval);
     }
 
@@ -30,15 +30,22 @@ public class GameManager : MonoBehaviour
         Instantiate(_targetPrefab, pos, Quaternion.identity);
 
         _totalTargetsSpawned++;
+        UpdateUIText();
         Debug.Log($"Targets spawned: {_totalTargetsSpawned}");
     }
     
-    Vector3 GetRandomWorldPosInView()
+    private Vector3 GetRandomWorldPosInView()
     {
         float u = Random.Range(0.1f, 0.9f);
         float v = Random.Range(0.1f, 0.9f);
         float zFromCamera = 10f;
 
         return _cam.ViewportToWorldPoint(new Vector3(u, v, zFromCamera));
+    }
+    
+    private void UpdateUIText()
+    {
+        if (_targetsSpawnedText != null)
+            _targetsSpawnedText.text = $"Targets spawned: {_totalTargetsSpawned}";
     }
 }
