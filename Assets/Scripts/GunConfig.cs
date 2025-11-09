b5c6f8244ddecb2f9621d97df052e58b29febf31
Ilia Rodkin <rid843@mail.ru>
2025-11-09 14:16:01 +0700

    feat: Add GunConfig SO and first config

---
+++ Assets/Scripts/GunConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "GunConfig", menuName = "Configs/GunConfig")]
+    public class GunConfig : ScriptableObject
+    {
+        [SerializeField][Min(0f)] private float _projectileSpeed = 20f;
+        [SerializeField][Min(0f)] private float _fireCooldown = 0.5f;
+        [SerializeField][Min(0f)] private int _damage = 10;
+        
+        public float ProjectileSpeed => _projectileSpeed;
+        public float FireCooldown => _fireCooldown;
+        public int Damage => _damage;
+    }
+}
