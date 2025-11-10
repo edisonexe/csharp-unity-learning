6771299cccd2b77b32b26514266fc349ada2fa5d
Ilia Rodkin <rid843@mail.ru>
2025-11-10 23:39:58 +0700

    feat: Add GameEvents class

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameEvents : MonoBehaviour
+    {
+        public event Action OnShotFired;
+        public event Action OnTargetHit;
+        public event Action OnTargetDestroyed;
+        
+        public void ShotFired() => OnShotFired?.Invoke();
+        public void TargetHit() => OnTargetHit?.Invoke();
+        public void TargetDestroyed() => OnTargetDestroyed?.Invoke();
+    }
+}
