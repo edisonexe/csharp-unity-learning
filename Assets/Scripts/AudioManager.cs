7f7126200dfef6bf5c73bc7eeb9012d0d3a53e35
Ilia Rodkin <rid843@mail.ru>
2025-11-11 00:00:13 +0700

    refactor: Add event unsubscription on disable

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -19,6 +19,12 @@ namespace DefaultNamespace
             _events.OnTargetHit += TargetHit;
         }
 
+        private void OnDisable()
+        {
+            _events.OnShotFired -= ShotFired;
+            _events.OnTargetHit -= TargetHit;
+        }
+        
         private void TargetHit() => _audioSource.PlayOneShot(_targetHitClip);
         
         private void ShotFired() => _audioSource.PlayOneShot(_shootClip);
