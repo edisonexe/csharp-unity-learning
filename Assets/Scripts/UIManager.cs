dc1490e3b7fe87253f395c36965e924b2a86bba9
Ilia Rodkin <rid843@mail.ru>
2025-11-10 23:58:32 +0700

    feat: Add UIManager class

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class UIManager : MonoBehaviour
+    {
+        [Header("UI Texts")]
+        [SerializeField] private Text _targetsSpawnedText;
+        [SerializeField] private Text _shotsText;
+        [SerializeField] private Text _targetHitsText;
+        
+        [Header("GameEvents")]
+        [SerializeField] private GameEvents _events;
+
+        private int _shots;
+        private int _hits;
+        
+        private void OnEnable()
+        {
+            _events.OnShotFired += ShotsFired;
+            _events.OnTargetHit += TargetHit;
+        }
+
+        private void OnDisable()
+        {
+            _events.OnShotFired -= ShotsFired;
+            _events.OnTargetHit -= TargetHit;
+        }
+        
+        private void TargetHit()
+        {
+            _hits ++;
+            if (_targetHitsText)
+                _targetHitsText.text = $"Hits: {_hits}";
+        }
+
+        private void ShotsFired()
+        {
+            _shots++;
+            if (_shotsText)
+                _shotsText.text = $"Shots {_shots}";
+        }
+    }
+}
