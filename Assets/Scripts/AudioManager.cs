2130a1bedc34ab6ef4639dba3cf08591ad7c1fbb
Ilia Rodkin <rid843@mail.ru>
2025-11-10 23:48:29 +0700

    feat: Add methods for play sounds in AudioManager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,7 +1,26 @@
-﻿namespace DefaultNamespace
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
 {
-    public class AudioManager
+    public class AudioManager : MonoBehaviour
     {
+        [Header("Audio Settings")]
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip _shootClip;
+        [SerializeField] private AudioClip _targetHitClip;
+
+        [Header("GameEvents")]
+        [SerializeField] private GameEvents _events;
+
+        private void OnEnable()
+        {
+            _events.OnShotFired += ShotFired;
+            _events.OnTargetHit += TargetHit;
+        }
+
+        private void TargetHit() => _audioSource.PlayOneShot(_targetHitClip);
         
+        private void ShotFired() => _audioSource.PlayOneShot(_shootClip);
     }
 }
