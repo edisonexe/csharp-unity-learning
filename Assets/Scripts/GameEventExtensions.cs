46cf04ccf6ae9070c1af11bc553d787fcade7c58
Ilia Rodkin <rid843@mail.ru>
2025-12-02 19:40:12 +0700

    feat: Add GameEventExtensions class

---
+++ Assets/Scripts/GameEventExtensions.cs
@@ -0,0 +1,10 @@
+﻿public static class GameEventExtensions
+{
+    public static string ToLogString(this GameEvent e)
+    {
+        if (e == null) return string.Empty;
+        
+        return $"<color=blue>[{e.Time:HH:mm:ss}]</color> " +
+               $"<color=black><b> [{e.Type.ToString()}]</b> {e.Description} </color>";
+    }
+}
