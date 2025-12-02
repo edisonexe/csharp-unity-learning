b84a16ae9034fd88cc9898a8afa7c6432f705a48
Ilia Rodkin <rid843@mail.ru>
2025-12-02 14:21:13 +0700

    feat: Add GameEvent class and GameEventType enum

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public record GameEvent
+{
+    public GameEventType Type { get; }
+    public DateTime Time { get; }
+    public string Description { get; }
+
+    public GameEvent(GameEventType type, DateTime time, string description)
+    {
+        Type = type;
+        Time = time;
+        Description = description;
+    }
+}
---
+++ Assets/Scripts/GameEventType.cs
@@ -0,0 +1,7 @@
+public enum GameEventType
+{
+    BattleStart, 
+    ItemPicked, 
+    WeatherChanged, 
+    EnemySpotted
+}
