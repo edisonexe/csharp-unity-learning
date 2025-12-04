de5c41ba7039050511e78e25ad57e1c8dace8dc8
Ilia Rodkin <rid843@mail.ru>
2025-12-04 16:05:02 +0700

    feat: Add NewsDisplay and StatusTextDisplay SO classes

---
+++ Assets/Scripts/Configs/NewsDisplayConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Configs
+{
+    [CreateAssetMenu(fileName = "NewsDisplayConfig", menuName = "Configs/News Display Config")]
+    public class NewsDisplayConfig : ScriptableObject
+    {
+        [Header("Colors")]
+        [SerializeField] private Color _titleColor = Color.white;
+        [SerializeField] private Color _contentColor = Color.white;
+        [SerializeField] private Color _dateColor = Color.gray;
+
+        public Color TitleColor => _titleColor;
+        public Color ContentColor => _contentColor;
+        public Color DateColor => _dateColor;
+    }
+}
---
+++ Assets/Scripts/Configs/StatusTextDisplayConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Configs
+{
+    [CreateAssetMenu(fileName = "StatusTextDisplayConfig", menuName = "Configs/Status Text Display Config")]
+    public class StatusTextDisplayConfig : ScriptableObject
+    {
+        [SerializeField] private Color _successColor = Color.green;
+        [SerializeField] private Color _errorColor = Color.red;
+        [SerializeField] private Color _warningColor = Color.yellow;
+        [SerializeField] private Color _infoColor = Color.black;
+
+        public Color SuccessColor => _successColor;
+        public Color ErrorColor => _errorColor;
+        public Color WarningColor => _warningColor;
+        public Color InfoColor => _infoColor;
+    }
+}
