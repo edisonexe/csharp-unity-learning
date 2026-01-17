628b5b97bcd0ccd9eb9419f073a97230d2715d29
Ilia Rodkin <rid843@mail.ru>
2026-01-17 18:24:55 +0700

    feat: Add SpawnService and IEnemyFactory

---
+++ Assets/_Scripts/Services/SpawnService.cs
@@ -0,0 +1,42 @@
+﻿using _Scripts.EventBus;
+using _Scripts.Spawning;
+
+namespace _Scripts.Services
+{
+    public class SpawnService
+    {
+        private readonly IEnemyFactory _factory;
+        private readonly float _interval;
+        private float _timer;
+
+        public SpawnService(IEnemyFactory factory, float intervalSec)
+        {
+            _factory = factory;
+            _interval = intervalSec;
+        }
+        
+        public void Enable() => EventBus.EventBus.Subscribe(HandleGameEvent);
+        
+        public void Disable() => EventBus.EventBus.Unsubscribe(HandleGameEvent);
+        
+        public void Tick(float deltaTime)
+        {
+            _timer += deltaTime;
+            if (_timer < _interval) return;
+            _timer = 0f;
+            Spawn();
+        }
+
+        private void Spawn()
+        {
+            _factory.SpawnOne();
+            EventBus.EventBus.Raise(GameEventType.EnemySpawned, 1);
+        }
+        
+        private void HandleGameEvent(GameEventType type, int value)
+        {
+            // Подписчик по ТЗ
+        }
+
+    }
+}
---
+++ Assets/_Scripts/Spawning/IEnemyFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace _Scripts.Spawning
+{
+    public interface IEnemyFactory
+    {
+        public void SpawnOne();
+    }
+}
