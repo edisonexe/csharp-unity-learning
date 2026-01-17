107b548ed7fcf4d79e3ad5546977242301d56ef5
Ilia Rodkin <rid843@mail.ru>
2026-01-17 18:25:22 +0700

    feat: Add EnemySpawner

---
+++ Assets/_Scripts/Spawning/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using _Scripts.Spawning;
+using UnityEngine;
+
+public sealed class EnemySpawner : MonoBehaviour, IEnemyFactory
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private List<Transform> spawnPoints;
+
+    public void SpawnOne()
+    {
+        if (spawnPoints == null || spawnPoints.Count == 0)
+        {
+            Debug.LogWarning("[EnemySpawner]: Нет точек спавна");
+            return;
+        }
+
+        Transform point = GetRandomSpawnPoint();
+        Instantiate(enemyPrefab, point.position, point.rotation);
+    }
+
+    private Transform GetRandomSpawnPoint()
+    {
+        var index = Random.Range(0, spawnPoints.Count);
+        return spawnPoints[index];
+    }
+}
