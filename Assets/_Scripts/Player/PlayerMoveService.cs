7642f4975ff6afa09ca89525f94658cf1d47fa38
Ilia Rodkin <rid843@mail.ru>
2026-01-18 19:25:29 +0700

    feat: Add player moving and import TMP

---
+++ Assets/_Scripts/Player/PlayerMoveService.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Player
+{
+    public sealed class PlayerMoveService
+    {
+        private readonly float _speed;
+
+        public PlayerMoveService(float speed) => _speed = speed;
+
+        public Vector3 CalculateDelta(Vector2 input, float dt)
+        {
+            if (input.sqrMagnitude < 0.0001f) return Vector3.zero;
+
+            var dir = new Vector3(input.x, 0f, input.y).normalized;
+            return dir * (_speed * dt);
+        }
+    }
+}
---
+++ Assets/_Scripts/Player/PlayerView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Scripts.Player
+{
+    public sealed class PlayerView : MonoBehaviour
+    {
+        private Vector2 _moveInput;
+        public Vector2 MoveInput => _moveInput;
+    
+        public void OnMove(InputAction.CallbackContext ctx) => _moveInput = ctx.ReadValue<Vector2>();
+
+        public void ApplyMove(Vector3 delta) => transform.position += delta;
+    }
+}
